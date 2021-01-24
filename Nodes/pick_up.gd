7553e67061563e133cbc5c4984d6829b571a9f3b
david <davidpedro.chico@gmail.com>
2021-01-24 18:49:06 +0100

    Pick Ups animation

--- Nodes/pick_up.gd
+++ Nodes/pick_up.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 class_name PickUp
 
@@ -10,9 +10,29 @@ export var weight = 1
 var player_flag = true
 var enemy_flag = false
 
+
+var motion = Vector2()
+var acc = Vector2()
+var energy = Vector2()
+
+
+var y_floor = 0
+
+var gravity = 4000
+var throw_force = 80000
+var throw_dir = Vector2(-1, -2).normalized()
+
+
+onready var collisionShape = $CollisionShape2D
+
+
+
 func _ready():
 	pass
 
+
+func _physics_process(delta):
+	move(delta)
 
 
 func pick(body):
@@ -24,6 +44,8 @@ func pick(body):
 		body.pickUps.add_child(self)
 		body.update_weight_velocity()
 		enemy_flag = false
+		
+		collisionShape.disabled = true
 
 
 func throw(player):
@@ -37,31 +59,49 @@ func throw(player):
 	
 	player.update_weight_velocity()
 	
-	global_position = player.pickUpSpawn.global_position + player.sprite_direction * throw_displacement
-	
 	var tween = Tween.new()
 	add_child(tween)
 	
 	var ini_pos = player.pickUpSpawn.global_position
 	
-	tween.interpolate_property(self, "global_position:x",
-		ini_pos.x, 
-		ini_pos.x + -1 * player.sprite_direction * throw_displacement.x, 
-		1.0,
-		Tween.TRANS_CIRC, Tween.EASE_OUT)
+	collisionShape.disabled = false
 	
-	tween.interpolate_property(self, "global_position:y",
-		ini_pos.y, 
-		ini_pos.y + throw_displacement.y, 
-		1.0,
-		Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	global_position = player.pickUpSpawn.global_position
+	acc = throw_dir * throw_force
+	acc.x = acc.x * player.sprite_direction
+	y_floor = ini_pos.y + throw_displacement.y
 	
-	tween.start()
+#	tween.interpolate_property(self, "global_position:x",
+#		ini_pos.x, 
+#		ini_pos.x + -1 * player.sprite_direction * throw_displacement.x, 
+#		1.0,
+#		Tween.TRANS_CIRC, Tween.EASE_OUT)
+#
+#	tween.interpolate_property(self, "global_position:y",
+#		ini_pos.y, 
+#		ini_pos.y + throw_displacement.y, 
+#		1.0,
+#		Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+#
+#	tween.start()
+#
+#	yield(tween, "tween_completed")
 
-	yield(tween, "tween_completed")
-	player_flag = true
-	enemy_flag = true
 	
+
+
+func move(delta):
+	if motion.length_squared() > 10 or acc.length_squared() > 0:
+		motion += delta * acc
+		acc = Vector2()
+		motion += delta * Vector2(0, gravity)
+
+		motion = move_and_slide(motion)
+		
+		if position.y > y_floor:
+			motion = Vector2()
+			player_flag = true
+			enemy_flag = true
 	
 
 
