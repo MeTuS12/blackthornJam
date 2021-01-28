183c2e9c9714040cbf7f13b0b8cc12166a903a10
david <davidpedro.chico@gmail.com>
2021-01-28 11:08:50 +0100

    Throne

--- Nodes/Anchor.gd
+++ Nodes/Anchor.gd
@@ -1,11 +1,9 @@
 extends Position2D
 
 onready var player = get_node("../Player")
-onready var tortise = get_node("../Player")
+onready var target2 = get_node("../Player")
 
 
-var target_pos_x
-var target_pos_y
 
 var flag_second_target = false
 
@@ -18,12 +16,18 @@ func remove_second_target():
 	flag_second_target = false
 
 
+func calc_zoom():
+	pass
+
+
 func _process(_delta):
+	var target = Vector2()
+	
 	if not flag_second_target:
-		var target = player.global_position
-		target_pos_x = int(lerp(global_position.x, target.x, .035))
-		target_pos_y = int(lerp(global_position.y, target.y, .035))
-		global_position = Vector2(target_pos_x, target_pos_y)
+		target = player.global_position
 	else:
-		pass
+		target = player.global_position # Media con el target 2
+	
+	global_position.x = int(lerp(global_position.x, target.x, .035))
+	global_position.y = int(lerp(global_position.y, target.y, .035))
 
